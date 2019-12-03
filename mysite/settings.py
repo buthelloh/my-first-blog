6eb872796ada58b50c4986cd4b66245ea2f507b6
J.HS <buthelloh@gmail.com>
2019-12-03 13:50:48 +0900

    change the settings.py debug=fall

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'acx+@gjoa-tbl_m@b@oqgc-x590upx0amo0$u9x_guch8=vi6('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
